4f3683984c36c6909edf51012c8e9e12952ad21d
James Bland <jammydodger8888@gmail.com>
2026-07-23 21:07:46 +0100

    feat: skills gallery v1 added

--- src/app/skills/Page.tsx
+++ src/app/skills/Page.tsx
@@ -1,6 +1,44 @@
 export const Page = () =>
 {
     return (
-        <p>Skills</p>
+        <div className="cv-page-shell">
+            <div className="cv-page">
+                <div className="cv-header">
+                    <div className="cv-name">Skills</div>
+                </div>
+                <section className="cv-section">
+                    <div className="cv-heading">Programming languages</div>
+                    <strong>c++, c#, javascript, typescript</strong>
+                    <p>I developed my 2D game shroom and doom 🧟 in C++. I love the structure that header and implementation files give</p>
+                    <p>I have developed a few games and projects in C# while using unity. These games include Grid runner, Elf defence and silly rockets.</p>
+                    <p>I have had a love for JavaScript since college, i stepped up my knowledge of JavaScript and game development in general when i made Knightly Knockout ⚔️, a multiplayer sword fighting game 🤺on the web. 🌐</p>
+                    <div className="cv-heading">Web technologies</div>
+                    <p>When making knightly knockout i used the following web technologies</p>
+                    <ul>
+                        <li>Three.js</li>
+                        <li>Ammo.js</li>
+                        <li>Socket.io</li>
+                        <li>Express</li>
+                        <li>Node</li>
+                        <li>MySQL</li>
+                        <li>html</li>
+                        <li>css</li>
+                    </ul>
+                    <div className="cv-heading">Game engines</div>
+                    <strong>Unreal Engine, Unity</strong>
+                    <p>I 💖Unreal engine its just so cool 😎. The visuals are amazing, blueprints make developing new gameplay features feels super fast and easy and the free content such as mega scans and metahuman really sweeten the deal! And that's not to mention nanite and lumen which consistently completely blow my mind 🤯i have almost entirely used blueprints to program it, however i did use some C++ when creating my game Freight as it needed to connect to a database to save high scores.</p>
+                    <p>Unity definitely  has a special place in my heart however. 🥰 we used unity in Year 1 at university to make my second ever game Grid runner, i also made my first ever 3d game in unity, working as part of a team, elf defence. I love that you write C# code to make your own custom components. And i cant quite put my finger on it just feels so good to make games in unity.</p>
+                    <div className="cv-heading">Source control</div>
+                    <strong>Git and Gitub</strong>
+                    <p>Working on my game Tiny Tavern with Bryce Tingle was what kicked off my proficiency with GitHub. Using GitHub to source control an unreal project is very tricky, but has been an almost trial by fire experience to get me proficient at using GitHub to source control my projects. I have used it to source C++ projects and web projects with no problems, creating branches for new features and merging and deleting those branches when the feature is complete.</p>
+                    <p>The experience which has solidified my confidence using git the most was working as a 2 man team to manage the source control of an unreal project with a team of 38 developers/artists as part of a module at University. And while there was challenges presented along the way the project was successfully source controlled throughout its development.</p>
+                    <div className="cv-heading">3D softwares</div>
+                    <strong>Blender, Maya, Substance Painter</strong>
+                    <p>Blender was my first exposure to anything games development related. I first discovered it in 2020 and followed the Blender Guru's doughnut tutorial (like everyone else did 😉), i absolutely loved it. i am terrible at traditional art but for some reason 3D art clicked and i spend the summer before college learning blender. Then after college i jumped back on the proverbial horse and made loads of renders in the summer before university. Now i use it as a tool in my game development.</p>
+                    <p>I learned Maya in year 1 at university. modelling is very enjoyable in maya and i learnt a lot about good topology which i had no clue about before. I know its lazy but also love the auto UV unwrap tool in maya 🙊. Currently i only know about rigging and animation in blender but would love to learn how it works in maya!</p>
+                    <p>Substance painter kinda feels like cheating to be honest. Its really fun to use and you can create quality looking assets in no time at all with their smart materials. i would Definity like to expand my skills in this.</p>
+                </section>
+            </div>
+        </div>
     )
 }
